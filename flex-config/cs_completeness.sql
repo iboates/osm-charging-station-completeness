8020d2dcdc56eae7c4057f55a5c5ab43cae21021
Isaac Boates <iboates@gmail.com>
2023-03-12 16:59:13 +0100

    a

--- flex-config/cs_completeness.sql
+++ flex-config/cs_completeness.sql
@@ -2,11 +2,24 @@ drop table if exists cs_completeness;
 create table cs_completeness as (
     select distinct on (cs.node_id)
         cs.node_id as node_id,
-        case when cs.capacity is not null then true else false end as capacity,
+        case when cs.brand is not null then true else false end as brand,
         case when cs.operator is not null then true else false end as operator,
-        case when s.type is not null then true else false end as socket_type,
-        case when s.number is not null then true else false end as socket_number,
-        case when s.output is not null then true else false end as socket_output,
+        case when cs.capacity is not null then true else false end as capacity,
+        case when cs.authentication is not null then true else false end as authentication,
+        case when cs.fee is not null then true else false end as fee,
+        case when cs.parking_fee is not null then true else false end as parking_fee,
+        case when cs.maxstay is not null then true else false end as maxstay,
+        case when cs.opening_hours is not null then true else false end as opening_hours,
+        case when cs.payment is not null then true else false end as payment,
+        case when cs.socket is not null then true else false end as socket,
+        case when cs.voltage is not null then true else false end as voltage,
+        case when cs.output is not null then true else false end as output,
+        case when cs.current is not null then true else false end as current,
+        case when s.type is not null then true else false end as ":socket:type",
+        case when s.number is not null then true else false end as ":socket:type:number",
+        case when s.output is not null then true else false end as ":socket:type:output",
+        case when s.voltage is not null then true else false end as ":socket:type:voltage",
+        case when s.current is not null then true else false end as ":socket:type:current",
         st_centroid(cs.geom) as geom
     from
         charging_station cs
--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import psycopg2 as pg2
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 from tqdm import tqdm
+import sqlalchemy as sa
 
 
 def _filter_pbf(pbf, filtered_pbf):
@@ -73,10 +74,9 @@ def _osm2pgsql(pbf, database, username, password, schema, host, port, flex_confi
         args = [*args[:-2], "--slim", args[-1]]
     sp.run(args, env={**os.environ.copy(), "PGPASSWORD": password})
 
-def _execute_sql(conn, sql_file):
+def _execute_sql(engine, sql_file):
 
-    with conn.cursor() as cur:
-        cur.execute(open(sql_file).read())
+    engine.execute(open(sql_file).read())
 
 
 class CLI:
@@ -87,6 +87,7 @@ class CLI:
             self.conn = pg2.connect(host=os.getenv("DB_HOST"), port=os.getenv("DB_PORT"), dbname=os.getenv("DB_NAME"),
                                user=os.getenv("DB_USER"), password=os.getenv("DB_PASS"))
             self.conn.autocommit = True
+            self.engine = sa.create_engine(f'postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASS")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}')
         except pg2.OperationalError:
             warnings.warn("Could not connect to PostGIS database.")
             self.conn = None
@@ -121,6 +122,72 @@ class CLI:
                    host=os.getenv("DB_HOST"), port=os.getenv("DB_PORT"), flex_config=flex_config)
         _execute_sql(self.conn, "flex-config/country_boundaries_to_polygon.sql")
         _execute_sql(self.conn, "flex-config/cs_completeness.sql")
+
+
+    def execute_sql(self, sql_file):
+
+        with self.engine.connect() as connection:
+            connection.execute(sa.text(open(sql_file).read()))
+
+    def summarize(self, sql_file=None, tags=None):
+
+        if sql_file is None:
+            sql_file = "sql/get_completeness.sql"
+
+        if tags is None:
+            tags = [
+                "brand",
+                "operator",
+                "capacity",
+                "authentication",
+                "fee",
+                "parking_fee",
+                "maxstay",
+                "opening_hours",
+                "payment",
+                "socket",
+                "voltage",
+                "output",
+                "current",
+            ]
+
+        ALLOWED_TAGS = [
+            "brand",
+            "operator",
+            "capacity",
+            "authentication",
+            "fee",
+            "parking_fee",
+            "maxstay",
+            "opening_hours",
+            "payment",
+            "socket",
+            "voltage",
+            "output",
+            "current",
+        ]
+
+        SOCKET_TAGS = {
+            "socket": ":socket:type",
+            "number": ":socket:type:number",
+            "voltage": ":socket:type:output",
+            "output": ":socket:type:voltage",
+            "current": ":socket:type:current"
+        }
+
+        with open(sql_file) as f:
+            query = f.read()
+        with open("sql/tag_fragment.sql") as f:
+            tag_fragment = f.read()
+
+        tag_fragment_replacements = []
+        for tag in tags:
+            if tag not in ALLOWED_TAGS:
+                raise ValueError(f"Invalid tag `{tag}`")
+            tag_fragment_replacements.append(tag_fragment.replace("[[TAG_VALUE]]", tag))
+        query = query.replace("[[TAG_FRAGMENTS]]", ",\n\n UNION ALL \n\n".join(tag_fragment_replacements))
+
+        res = pd.read_sql(sa.text(query), self.engine)
 
 
 if __name__ == "__main__":
